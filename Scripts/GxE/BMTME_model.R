afe9e0a94b62a80b066598740b04b8dbcaaf55d3
Kenia Segura Abá <keniaesegura@gmail.com>
2022-11-16 14:15:27 -0500

    Script for multi-environment multi-trait models

--- Scripts/GxE/BMTME_model.R
+++ Scripts/GxE/BMTME_model.R
@@ -1,5 +1,6 @@
-# Bayesian multiple-trait multiple-environment model for GxE anlysis
+# Bayesian multiple-environment model for GxE anlysis
 
+library(data.table)
 library(BMTME)
 
 ### Read in data ###
@@ -13,20 +14,41 @@ X <- as.matrix(X, rownames=1, colnames=1)
 
 # Phenotype data
 y <- read.csv('/mnt/home/seguraab/Shiu_Lab/Project/Data/Peter_2018/pheno.csv')
-y_melt <- melt(y, id.vars='Unnamed..0') # pivot longer
-colnames(y_melt) <- c('ID', 'Env', 'y')
+#y_melt <- melt(y, id.vars='Unnamed..0') # pivot longer
+#colnames(y_melt) <- c('ID', 'Env', 'y')
 
 # train-test split
 `%notin%` <- Negate(`%in%`)
 X_train <- X[rownames(X) %notin% Test,]
 X_test <- X[Test,]
-y_train <- y_melt[y_melt$ID %notin% Test,] 
-y_test <- y_melt[y_melt$ID %in% Test,]
+#y_train <- y_melt[y_melt$ID %notin% Test,] 
+#y_test <- y_melt[y_melt$ID %in% Test,]
+y_train <- y[y$Unnamed..0 %notin% Test,]
+y_test <- y[y$Unnamed..0 %in% Test,]
 
+########### Calculate the Genomic Relationship Matrix ###########
+# Method 1: tcrossprod(X)/# of markers
+# G <- tcrossprod(X)/ncol(X) 
+G_train <- tcrossprod(X_train)/ncol(X_train) 
 
-### Bayesian Multi-Environment Model ###
-G <- X%*%t(X)# VanRaden Genomic Relationship Matrix
+# Method 2: tcrossprod(centered X)/(2*sum(pq))
+# phat <- colMeans(X_train)/2 # minor allele frequency
+# X_train2 <- scale(X_train, center=TRUE, scale=FALSE)
+# k <- 2*sum(phat*(1-phat)) # denominator
+# G_train2 <- tcrossprod(X2)/k
 
-LG <- cholesky(X)
+# # Method 3: tcrossprod(Z)/# of markers
+# Z_train <- scale(X_train, center=TRUE, scale=TRUE)
+# G_train3 <- tcrossprod(Z_train)/ncol(Z_train)
 
-BME(Y=y_train, Z1=, nIter=10000, burnIn=1000,)
+########### Fit Bayesian Multi-Environment Model ###########
+LG <- cholesky(G_train) #?
+ZG <- model.matrix(~0 + as.factor(y_train$Unnamed..0)) #?
+Z.G <- ZG %*% LG #?
+
+fm <- BME(Y=as.matrix(y_train[-1]), # pheno data in each env 
+    Z1=Z.G, # genetic effects
+    nIter=10000, # iterations
+    burnIn=5000,
+    thin=2, 
+    bs=625)
