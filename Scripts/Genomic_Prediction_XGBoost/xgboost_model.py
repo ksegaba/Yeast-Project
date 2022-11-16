43551b4d15472fdf654b12f455d73cbfb5cf42f1
Kenia Segura Abá <keniaesegura@gmail.com>
2022-11-16 14:15:45 -0500

    Script for XGBoost model

--- Scripts/Genomic_Prediction_XGBoost/xgboost_model.py
+++ Scripts/Genomic_Prediction_XGBoost/xgboost_model.py
@@ -1,10 +1,31 @@
 """
-XGBoost for regression
+XGBoost for regression on SNP, ORF, and CNO data from Peter et al. 2018
 
-Kenia Segura Aba
+# Required Inputs
+    -X      Path to feature matrix
+    -Y      Path to label matrix
+    -test   List of test instances
+    -trait  Column name of target trait in Y matrix
+    -save   Path to save output files
+    # Optional
+    -type   Feature types (e.g. SNP (default), ORF, CNO)
+    -fold   k folds for Cross-Validation (default is 5)
+    -n      Number of CV repetitions (default is 10)
+
+# Outputs (prefixed with <type>_<trait>)
+    _GridSearch.csv     Grid Search CV R-sq scores
+    _lm_test.pdf        Regression plot of predicted and actual test labels
+    _model.save         XGBoost model
+    _imp.csv            Feature importance scores
+    _top20.pdf          Plot of top 20 features' importance scores
+    _cv_results.csv     Cross-validation results (various metrics)
+    _test_results.csv   Evaluation results (various metrics)
+    RESULTS_xgboost.txt Aggregated results (various metrics)    
 """
+__author__ = "Kenia Segura Abá"
+
 from configparser import ExtendedInterpolation
-import sys, os, time
+import sys, os, argparse, time
 from datetime import datetime
 import random
 from operator import index
@@ -15,148 +36,222 @@ import pickle
 import xgboost as xgb
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.metrics import explained_variance_score
+from sklearn.model_selection import RepeatedKFold
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import cross_val_predict
 from sklearn.model_selection import GridSearchCV
-random.seed(123)
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Need to add args
 # Need trait arg bc the for loop may cause it to crash on the command line
 # So I need a slurm script to keep track 
 
-def main():
+def tune_model(xg_reg, X_train, y_train, data_type, trait, fold):
+    """ Hyperparameter sweep using grid search with cross-validation """
+    start = time.time()
+
+    # Hyperparameter tuning
+    parameters = {"gamma": [0,0.01], # min split loss
+                "eta":[0.3, 0.1, 0.03], # learning rate
+                "max_depth":[3, 6, 7], # tree depth
+                "subsample": [0.5, 1], # instances per tree
+                "colsample_bytree": [0.5, 1], # features per tree
+                "n_estimators": [100, 300, 500]} # sample training instances
+    gs = GridSearchCV(estimator=xg_reg,
+                param_grid=parameters,
+                cv=fold,           # cross validation folds
+                scoring='r2',   # find model with the best R-squared
+                verbose=2,
+                n_jobs=10)
+    fitted_model = gs.fit(X_train, y_train) # fit the model
+
+    run_time = time.time() - start
+    print("GridSearch Run Time: %f" % (run_time))
+
+    # Write results to file
+    gs_results = pd.DataFrame(gs.cv_results_)
+    gs_results.to_csv(f"{args.save}/{data_type}_{trait}_GridSearch.csv")
+
+    print("Best parameters: ", fitted_model.best_params_)
+
+    return (fitted_model.best_params_, gs.best_estimator_)
+
+
+def xgb_reg(trait, fold, n, data_type):
+    """ Train XGBoost Regression Model """
+    print(trait)
+    y = Y[trait]
+
+    # Train-test split
+    X_train = X.loc[~X.index.isin(test[0])]
+    X_test = X.loc[X.index.isin(test[0])]
+    y_train = y.loc[~y.index.isin(test[0])]
+    y_test = y.loc[y.index.isin(test[0])]
+
+    # Convert trainig set to Dmatrix data structure 
+    data_dmatrix = xgb.DMatrix(data=X_train,label=y_train)
+
+    # Instantiate XGBoost regressor object
+    xg_reg = xgb.XGBRegressor(random_state=random.seed(123))
+
+    # Hyperparameter tuning
+    best_params, best_model = tune_model(
+        xg_reg, X_train, y_train, data_type, trait, fold)
+
+    ################## Training with Cross-Validation ##################
+    results_cv = [] # hold performance metrics of cv reps
+    results_test = [] # hold performance metrics on test set
+
+    # Training with Cross-validation
+    # cv = RepeatedKFold(n_splits=fold, n_repeats=n, random_state=123)
+    # cv_scores = xgb.cv(
+    #     params=best_params,
+    #     dtrain=data_dmatrix,
+    #     num_boost_round=10,
+    #     stratified=True,
+    #     folds=cv,
+    #     metrics="rmse", # there is no r2_score equivalent
+    #     seed=123)
+
+    for j in range(0, n): # repeat cv 10 times
+        print(f"Running {j+1} of {n}")
+        
+        cv = StratifiedKFold(n_splits=fold) # stratified samples
+        cv_pred = cross_val_predict(
+            best_model, X_train, y_train, cv=fold, n_jobs=-1) # predictions
+
+        # Performance statistics on validation set
+        mse_val = mean_squared_error(y_train, cv_pred)
+        rmse_val = np.sqrt(mean_squared_error(y_train, cv_pred))
+        evs_val = explained_variance_score(y_train, cv_pred)
+        r2_val = r2_score(y_train, cv_pred)
+        cor_val = np.corrcoef(np.array(y_train), cv_pred)
+        print("Val MSE: %f" % (mse_val))
+        print("Val RMSE: %f" % (rmse_val))
+        print("Val R-sq: %f" % (r2_val))
+        print("Val PCC: %f" % (cor_val[0, 1]))
+        result_val = [mse_val, rmse_val, evs_val, r2_val, cor_val[0, 1]]
+        results_cv.append(result_val)
+
+    # Evaluate the model on the test set
+    #best_model.fit(X_train, y_train)
+    y_pred = best_model.predict(X_test)
+
+    # Performance on the test set
+    mse = mean_squared_error(y_test, y_pred)
+    rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+    evs = explained_variance_score(y_test, y_pred)
+    r2 = r2_score(y_test, y_pred)
+    cor = np.corrcoef(np.array(y_test), y_pred)
+    print("Test MSE: %f" % (mse))
+    print("Test RMSE: %f" % (rmse))
+    print("Test R-sq: %f" % (r2))
+    print("Test PCC: %f" % (cor[0, 1]))
+    result_test = [mse, rmse, evs, r2, cor[0, 1]]
+    results_test.append(result_test)
+
+    # Plot linear regression of actual and predicted test values
+    sns.regplot(x=y_pred, y=y_test, fit_reg=True, ci=95, seed=123, color="black")
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.title(f"{trait}")
+    plt.savefig(f"{args.save}/{data_type}_{trait}_lm_test.pdf", format="pdf")
+
+    # Save the fitted model to a file
+    filename = f"{args.save}/{data_type}_{trait}_model.save"
+    pickle.dump(best_model, open(filename, 'wb'))
+
+    # Save feature importance scores to file
+    importances = pd.DataFrame(best_model.feature_importances_)
+    importances.to_csv(f"{args.save}/{data_type}_{trait}_imp.csv")
+
+    # Plot feature importances
+    xgb.plot_importance(
+        best_model, grid="off", max_num_features=20, 
+        title=f"{trait} Feature Importances", xlabel="Weight")
+    plt.savefig(f"{args.save}/{data_type}_{trait}_top20.pdf", format="pdf")
+    
+    return (results_cv, results_test)
+
+
+if __name__ == "__main__":
+    # Argument parser
+    parser = argparse.ArgumentParser(
+        description="XGBoost Regression on SNP and ORF data")
+    
+    # Required input
+    req_group = parser.add_argument_group(title="Required Input")
+    req_group.add_argument(
+        "-X", help="path to feature data file", required=True)
+    req_group.add_argument(
+        "-Y", help="path to label data file", required=True)
+    req_group.add_argument(
+        "-test", help="path to file of test set instances", required=True)
+    req_group.add_argument(
+        "-trait", help="name of trait column in y dataframe", required=True)
+    req_group.add_argument(
+        "-save", help="path to save output files", required=True)
+    
+    # Optional input
+    req_group.add_argument(
+        "-type", help="data type of X matrix (e.g. SNP, ORF, CNO)", default="SNP")
+    req_group.add_argument(
+        "-fold", help="k number of cross-validation folds", default=5)
+    req_group.add_argument(
+        "-n", help="number of cross-validation repetitions", default=10)
+    
+    # Help
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit()
+    args = parser.parse_args() # Read arguments
+
     # Read in data
-    #X = dt.fread("/mnt/home/seguraab/Shiu_Lab/Project/Data/Peter_2018/geno.csv")
-    #X = dt.fread("/mnt/home/seguraab/Shiu_Lab/Project/Data/Peter_2018/ORFs_no_NA.csv") # ORF copy number
-    X = dt.fread("/mnt/home/seguraab/Shiu_Lab/Project/Data/Peter_2018/ORFs_pres_abs.csv") # ORF presence/absence
+    X = dt.fread(args.X)
     X = X.to_pandas()
     X.set_index(X.columns[0], inplace=True)
-    Y = pd.read_csv("/mnt/home/seguraab/Shiu_Lab/Project/Data/Peter_2018/pheno.csv", index_col=0)
-    test = pd.read_csv("/mnt/home/seguraab/Shiu_Lab/Project/Data/Peter_2018/Test.txt", sep="\t", header=None)
+    Y = pd.read_csv(args.Y, index_col=0)
+    test = pd.read_csv(args.test, sep="\t", header=None)
 
-    data_type = "ORF" # SNP  CNO
-    #tmp=["YPDSDS", "YPRIBOSE"]#"YPDFLUCONAZOLE", "YPDMV"] #"YPDANISO10", "YPDETOH"]#, , "YPDFORMAMIDE5"
-    tmp=['YPDBENOMYL500']
-    for trait in tmp:#Y.columns: #
-        print(trait)
-        y = Y[trait]
+    # Train the model
+    start = time.time()
+    results_cv, results_test = xgb_reg(
+        args.trait, int(args.fold), int(args.n), args.type)
+    run_time = time.time() - start
+    print("Training Run Time: %f" % (run_time))
 
-        # Convert to Dmatrix data structure 
-        data_dmatrix = xgb.DMatrix(data=X,label=y)
+    # Save results to file
+    results_cv = pd.DataFrame(
+        results_cv, 
+        columns=["MSE_val", "RMSE_val", "EVS_val", "R2_val", "PCC_val"])
+    results_cv.to_csv(f"{args.save}/{args.type}_{args.trait}_cv_results.csv")
+    results_test = pd.DataFrame(
+        results_test, 
+        columns=["MSE_test", "RMSE_test", "EVS_test", "R2_test", "PCC_test"])
+    results_test.to_csv(
+        f"{args.save}/{args.type}_{args.trait}_test_results.csv")
 
-        # Create train and test set
-        X_train = X.loc[~X.index.isin(test[0])]
-        X_test = X.loc[X.index.isin(test[0])]
-        y_train = y.loc[~y.index.isin(test[0])]
-        y_test = y.loc[y.index.isin(test[0])]
+    # Aggregate results and save to file
+    if not os.path.isfile(f"{args.save}/RESULTS_xgboost.txt"):
+        out = open(f"{args.save}/RESULTS_xgboost.txt", "a")
+        out.write("Date\tRunTime\tData\tTrait\tMSE_val\tMSE_val_sd\
+            \tRMSE_val\tRMSE_val_sd\tEVS_val\tEVS_val_sd\tR2_val\
+            \tR2_val_sd\tPCC_val\tPCC_val_sd\tMSE_test\tMSE_test_sd\
+            \tRMSE_test\tRMSE_test_sd\tEVS_test\tEVS_test_sd\tR2_test\
+            \tR2_test_sd\tPCC_test\tPCC_test_sd\n")
+        out.close()
 
-        ########## Parameter sweep using grid search ##########
-        start = time.time()
-        
-        gs_results = pd.DataFrame(columns=['mean_test_score', 'params'])
-        
-        """
-		GS_REPS = 10
-        for j in range(GS_REPS):
-            print("Round %s of %s" % (j + 1, GS_REPS))
-            
-            # Instantiate XGBoost regressor object
-            xg_reg = xgb.XGBRegressor()#random_state=random.seed(123))
-        
-            # Hyperparameter tuning
-            parameters = {"subsample":[0.5, 0.75, 1],
-                        "colsample_bytree":[0.5, 0.75, 1],
-                        "max_depth":[3, 10],
-                        "min_child_weight":[1, 5],
-                        "learning_rate":[0.3, 0.1, 0.03],
-                        "n_estimators":[100, 500]}
-            gs = GridSearchCV(estimator=xg_reg,
-                        param_grid=parameters,
-                        cv=5,           # cross validation folds
-                        scoring='r2',   # find model with the best R-squared
-                        verbose=1)
-            fitted_model = gs.fit(X_train, y_train) # fit the model
-            
-            # Add results to dataframe
-            j_results = pd.DataFrame(gs.cv_results_)
-            gs_results = pd.concat([gs_results, j_results[['params',
-                'mean_test_score']]])
-            
-        # Break params into seperate columns and save to file
-        gs_results2 = pd.concat([gs_results.drop(['params'], axis=1),
-            gs_results['params'].apply(pd.Series)], axis=1)
-        gs_results.to_csv("%s_%s_GridSearchFULL.txt"%(data_type, trait))
-
-        print("Best parameters: ", fitted_model.best_params_)
-        """
-		#######################################################
-        # Run XGBoost model
-        results_val = []
-        results_test = []
-        n = 1
-        for j in range(0, n):
-            print("Running %i of %i" % (j + 1, n))
-            #best_model = gs.best_estimator_
-            best_model = xgb.XGBRegressor()
-
-            # Cross-validation
-            cv_pred = cross_val_predict(estimator=best_model, X=X_train, y=y_train,
-                                        cv=5, n_jobs=-1)
-            
-            # Performance statistics from cross-validation
-            mse_val = mean_squared_error(y_train, cv_pred)
-            rmse_val = np.sqrt(mean_squared_error(y_train, cv_pred))
-            evs_val = explained_variance_score(y_train, cv_pred)
-            r2_val = r2_score(y_train, cv_pred)
-            cor_val = np.corrcoef(np.array(y_train), cv_pred)
-            print("Val RMSE: %f" % (rmse))
-            print("Val R-sq: %f" % (r2))
-            print("Val PCC: %f" % (cor_val))
-            result_val = [mse_val, rmse_val, evs_val, r2_val, cor_val[0, 1]]
-            results_val.append(result_val)
-
-            # Fit the model
-            best_model.fit(X_train, y_train)
-
-            # Apply the model to the test set
-            test_pred = best_model.predict(X_test)
-
-            # Performance on the test set
-            mse = mean_squared_error(y_test, test_pred)
-            rmse = np.sqrt(mean_squared_error(y_test, test_pred))
-            evs = explained_variance_score(y_test, test_pred)
-            r2 = r2_score(y_test, test_pred)
-            cor = np.corrcoef(np.array(y_test), test_pred)
-            print("Test RMSE: %f" % (rmse))
-            print("Test R-sq: %f" % (r2))
-            print("Test PCC: %f" % (cor))
-            result_test = [mse, rmse, evs, r2, cor[0, 1]]
-            results_test.append(result_test)
-
-            run_time = time.time() - start
-            print("Run Time: %f" % (run_time))
-
-        # Save the fitted model
-        filename = "%s_%s_model.save"%(data_type, trait)
-        pickle.dump(best_model, open(filename, 'wb'))
-
-        # Save results to file
-        if not os.path.isfile("/mnt/home/seguraab/Shiu_Lab/Project/Scripts/ \
-            Genomic_Prediction_XGBoost/RESULTS_xgboost.txt"):
-            out = open("/mnt/home/seguraab/Shiu_Lab/Project/Scripts/ \
-                Genomic_Prediction_XGBoost/RESULTS_xgboost.txt", "a")
-            out.write("Date\tRunTime\tData\tTrait\tMSE_val\tRMSE_val\tEVS_val \
-                \tR2_val\tPCC_val\tMSE_test\tRMSE_test\tEVS_test\tR2_test \
-                \tPCC_test\n")
-            out.close()
-
-        out = open("/mnt/home/seguraab/Shiu_Lab/Project/Scripts/ \
-            Genomic_Prediction_XGBoost/RESULTS_xgboost.txt", "a")
-        out.write("%s\t%s\t%s\t%s\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n"%
-        (datetime.now().strftime('%Y-%m-%d %H:%M:%S'),run_time,data_type,trait,
-            mse_val,rmse_val,evs_val,r2_val,cor_val,mse,rmse,evs,r2,cor))
-
-        # Feature importances
-        #importances = best_model.feature_importances_
-if __name__ == "__main__":
-    main()
+    out = open(f"{args.save}/RESULTS_xgboost.txt", "a")
+    out.write(f"\n{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\t\
+        {run_time}\t{args.type}\t{args.trait}\t\
+        {np.mean(results_cv.MSE_val)}\t{np.std(results_cv.MSE_val)}\t\
+        {np.mean(results_cv.RMSE_val)}\t{np.std(results_cv.RMSE_val)}\t\
+        {np.mean(results_cv.EVS_val)}\t{np.std(results_cv.EVS_val)}\t\
+        {np.mean(results_cv.R2_val)}\t{np.std(results_cv.R2_val)}\t\
+        {np.mean(results_cv.PCC_val)}\t{np.std(results_cv.PCC_val)}\t\
+        {np.mean(results_test.MSE_test)}\t{np.std(results_test.MSE_test)}\t\
+        {np.mean(results_test.RMSE_test)}\t{np.std(results_test.RMSE_test)}\t\
+        {np.mean(results_test.EVS_test)}\t{np.std(results_test.EVS_test)}\t\
+        {np.mean(results_test.R2_test)}\t{np.std(results_test.R2_test)}\t\
+        {np.mean(results_test.PCC_test)}\t{np.std(results_test.PCC_test)}")
