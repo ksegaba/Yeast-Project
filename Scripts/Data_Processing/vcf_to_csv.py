74e6e723e270bb2e5a3313bdb22156475c1aca8e
Kenia Segura Abá <keniaesegura@gmail.com>
2023-02-13 13:50:15 -0500

    Fixed typo on line 55

--- Scripts/Data_Processing/vcf_to_csv.py
+++ Scripts/Data_Processing/vcf_to_csv.py
@@ -4,8 +4,9 @@ Convert VCF file to CSV format
 Args:
     vcf (str): path to VCF file
     out (str): path to CSV file to save as
-
 """
+__author__ = "Kenia E. Segura Abá"
+
 import sys
 import argparse
 import warnings
@@ -16,7 +17,7 @@ def warn(*args, **kwargs):
     pass
 warnings.warn = warn
 
-if __name__=="__main__"
+if __name__=="__main__":
     # Arguments
     parser = argparse.ArgumentParser(description="Convert VCF to CSV format")
     req_group = parser.add_argument_group(title="REQUIRED INPUT")
@@ -26,17 +27,5 @@ if __name__=="__main__"
         parser.print_help()
         sys.exist(0)
     args = parser.parse_args()
-    
-    # Convert to PLINK format
-    vcf = args.vcf # vcf file
-    if vcf.endswith('.vcf'): # plink file to save as
-        plink = re.sub('.vcf', '.plink', vcf)
-    if vcf.endswith('.gvcf'):
-        plink = re.sub('.gvcf', '.plink', vcf)
-    os.system("" \
-        f"")
-
-    snp_info, sample_info, genotypes = pandas_plink.read_plink("/mnt/home/seguraab/Shiu_Lab/Collabs/Maize_GxE_Competition_Data/Training_Data/5_Genotype_Data_All_Years_maf005_maxmiss090_cleaned_BEAGLE_imputed.plink")
-    mat = genotypes.compute()
     
     # Convert to CSV
