deae208b755a7f8dc0974477cab361f35347e23b
Kenia Segura Abá <keniaesegura@gmail.com>
2023-03-15 13:53:55 -0400

    Peipei's code to generate original test set

---
+++ Scripts/Data_Processing/10_holdout_test_stratified.py
@@ -0,0 +1,47 @@
+"""
+Script to do a stratified K-fold train-test split.
+This script was used to generate a list of test set instances
+for all models in order to compare them.
+"""
+import sys,os
+import numpy as np
+import random
+import pandas as pd
+import imp
+from sklearn.model_selection import StratifiedKFold
+
+file = sys.argv[1] # your pheno matrix
+trait = sys.argv[2] # the target trait
+number = int(sys.argv[3]) # the fold you want to hold out as test set
+Y = pd.read_csv(file,header=0,index_col=0,sep=',')
+# Y_tem = Y.copy(deep=True) # I can't remember but Peipei may have added this after in her version of this code
+# Y_tem.index = range(1,(Y.shape[0]+1)) # I don't know why she added the 2 classes either.
+# Y_tem['Class'] = 0
+Y['Class'] = 1
+# df = pd.concat([Y,Y_tem],axis=0) # join row-wise, why do we need 2 classes?
+Tr_te = StratifiedKFold(n_splits=number, random_state=42,shuffle=True)
+nn = 0
+# for train_index, test_index in Tr_te.split(df.loc[:,trait],df.Class):
+# 	if nn == 0:
+# 		train_ID = df.iloc[train_index]
+# 		test_ID = df.iloc[test_index]
+# 		nn = 1
+for train_index, test_index in Tr_te.split(Y.loc[:,trait],Y.Class):
+	if nn == 0:
+		train_ID = Y.iloc[train_index]
+		test_ID = Y.iloc[test_index]
+		nn = 1
+	# train_ID = Y.iloc[train_index] # these were for testing purposes
+	# print(train_ID.shape)
+	# print(train_ID.head())
+	# test_ID = Y.iloc[test_index]
+	# print(test_ID.shape)
+	# print(test_ID.head())
+	# print(test_ID[test_ID.Class==1].index.to_list())
+
+test = test_ID[test_ID.Class==1].index.to_list()
+out = open('Test.txt','w')
+for t in test:
+	out.write('%s\n'%t)
+
+out.close()
