3c6fcd8dbe6a0a5026377a72dcfb4f430d671b9a
Kenia Segura Abá <keniaesegura@gmail.com>
2023-03-27 15:10:27 -0400

    convert vcf to matrix

--- Scripts/Data_Processing/01_convert_vcf_to_matrix.py
+++ Scripts/Data_Processing/01_convert_vcf_to_matrix.py
@@ -6,8 +6,14 @@ import pandas as pd
 
 df = pd.read_csv('1011Matrix_genotype_matrix',header=0,index_col=2,sep='\t')
 df = df.iloc[:,8:]
+<<<<<<< HEAD
+df = df.replace('0/0', -1)
+df = df.replace('0/1', 0)
+df = df.replace('1/1', 1)
+=======
 df = df.replace('0/0', -1) # homozygous reference allele
 df = df.replace('0/1', 0) # heterozygous
 df = df.replace('1/1', 1) # homozygous alternate allele
+>>>>>>> origin/main
 df = df.transpose()
 df.to_csv('1011GWAS_matrix.txt',header=True, index=True,sep=',')
