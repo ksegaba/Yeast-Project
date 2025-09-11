9ef2894255627ca8468125f78797407fb02cd77b
ksegaba <keniaesegura@gmail.com>
2025-09-11 09:27:46 -0400

    updated file names and results

--- Scripts/Data_Vis/0_Table_S6_rank_rho_btwn_variants.py
+++ Scripts/Data_Vis/0_Table_S6_rank_rho_btwn_variants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 ################################################################################
-# TABLE S5: SPEARMAN's RHO FEATURE RANK PERCENTILE CORRELATIONS BETWEEN VARIANTS
+# TABLE S6: Spearman's rho of feature rankings between genetic variant types
 ################################################################################
 
 import os
@@ -30,32 +30,28 @@ mapping = {"YPACETATE": "YP Acetate 2%", "YPD14": "YPD 14ยบC", "YPD40": "YPD 40ย
            "YPRIBOSE": "YP Ribose 2%", "YPGLYCEROL": "YP Glycerol 2%",
            "YPXYLOSE": "YP Xylose 2%", "YPSORBITOL": "YP Sorbitol 2%"}
 
-# SPEARMAN'S RHO CORRELATIONS BEWTEEN DATA TYPES
+# Load gene mapping files (to compare SNPs to ORFs)
 map_snps = pd.read_csv("Data/Peter_2018/biallelic_snps_diploid_and_S288C_genes_CORRECTED.tsv",
                        sep="\t", header=None, names=["snp", "chr", "pos", "gene"])
 map_orfs = pd.read_csv(
     "Data/Peter_2018/final_map_orf_to_gene_CORRECTED_16_removed.tsv", sep="\t")
 
 res = [["Model Type", "Importance Type", "Env",
-        "Comparison", "rho", "pval", "Num Shared Genes"]]
-
-for mod_type in ["baseline", "FS"]:
-    for imp_type in ["imp", "shap"]:
-        # _rank_per.tsv").to_pandas()
+        "Comparison", "rho", "pval", "NumSharedGenes"]]
+for mod_type in ["complete", "optimized"]:
+    for imp_type in ["gini", "shap"]:
         df_snp = dt.fread(
-            f"Scripts/Data_Vis/Section_4/RF_{mod_type}_{imp_type}_snp.tsv").to_pandas()
-        # _rank_per.tsv").to_pandas()
+            f"Scripts/Data_Vis/Section_3/RF_{mod_type}_{imp_type}_snp.tsv").to_pandas()
         df_pav = dt.fread(
-            f"Scripts/Data_Vis/Section_4/RF_{mod_type}_{imp_type}_pav.tsv").to_pandas()
-        # _rank_per.tsv").to_pandas()
+            f"Scripts/Data_Vis/Section_3/RF_{mod_type}_{imp_type}_pav.tsv").to_pandas()
         df_cnv = dt.fread(
-            f"Scripts/Data_Vis/Section_4/RF_{mod_type}_{imp_type}_cnv.tsv").to_pandas()
+            f"Scripts/Data_Vis/Section_3/RF_{mod_type}_{imp_type}_cnv.tsv").to_pandas()
         #
         # Calculate spearman's rho
-        if imp_type == "imp":
-            imp_score_type = "mean gini importance"
+        if imp_type == "gini":
+            imp_score_type = "gini importance"
         else:
-            imp_score_type = "mean absolute SHAP value"
+            imp_score_type = "average absolute SHAP value"
         #
         # Prep data for SNP to PAV/CNV comparisons
         tmp_snp = df_snp.loc[df_snp.gene != "intergenic", :]
@@ -79,9 +75,9 @@ for mod_type in ["baseline", "FS"]:
                 df = pd.concat([tmp_snp.loc[:, ["gene", env]].groupby("gene").max(),
                                 tmp_pav.loc[:, ["gene", env]].groupby("gene").max()],
                                ignore_index=False, axis=1).dropna()
-                df = df.loc[~df.eq(0).any(axis=1)]
+                df = df.loc[~df.eq(0).any(axis=1)]  # drop features with 0 vals
                 df = df.rank(pct=True)
-                if len(df) > 2:
+                if len(df) >= 2:
                     rho = df.corr(method=lambda x,
                                   y: spearmanr(x, y).statistic)
                     pval = df.corr(method=lambda x, y: spearmanr(x, y).pvalue)
@@ -101,7 +97,7 @@ for mod_type in ["baseline", "FS"]:
                                ignore_index=False, axis=1).dropna()
                 df = df.loc[~df.eq(0).any(axis=1)]
                 df = df.rank(pct=True)
-                if len(df) > 2:
+                if len(df) >= 2:
                     rho = df.corr(method=lambda x,
                                   y: spearmanr(x, y).statistic)
                     pval = df.corr(method=lambda x, y: spearmanr(x, y).pvalue)
@@ -123,7 +119,7 @@ for mod_type in ["baseline", "FS"]:
                 df = pd.concat([pav, cnv], ignore_index=False, axis=1).dropna()
                 df = df.loc[~df.eq(0).any(axis=1)]
                 df = df.rank(pct=True)
-                if len(df) > 2:
+                if len(df) >= 2:
                     rho = df.corr(method=lambda x,
                                   y: spearmanr(x, y).statistic)
                     pval = df.corr(method=lambda x, y: spearmanr(x, y).pvalue)
@@ -138,53 +134,55 @@ for mod_type in ["baseline", "FS"]:
                             None, None, None])
         del df_snp, df_pav, df_cnv, tmp_snp, tmp_pav, tmp_cnv
 
+
 res = pd.DataFrame(res)
 res.columns = res.iloc[0, :]
 res = res.iloc[1:, :]
 res.sort_values(by='rho', ascending=False, inplace=True)
-# res.to_csv("Scripts/Data_Vis/Section_4/Table_S5_rank_per_corr_btwn_data_types.tsv", sep="\t", index=False)
-res.to_csv("Scripts/Data_Vis/Section_4/Table_S5_rank_per_corr_btwn_data_types_with_n_shared_genes_CORRECTED.tsv",
+res.to_csv("Scripts/Data_Vis/Section_3/Table_S6_rank_per_corr_btwn_data_types_with_n_shared_genes.tsv",
            sep="\t", index=False)
 
 
 # Get the average overlap of shared genes between variant types across all 35 environments
 res = pd.read_csv(
-    "Scripts/Data_Vis/Section_4/Table_S5_rank_per_corr_btwn_data_types_with_n_shared_genes_CORRECTED.tsv", sep="\t")
+    "Scripts/Data_Vis/Section_4/Table_S6_rank_per_corr_btwn_data_types_with_n_shared_genes.tsv", sep="\t")
 res.insert(1, "imp_type", res.apply(
     lambda x: "gini" if "gini" in x["Importance Type"] else "SHAP", axis=1))
+res["NumSharedGenes"] = res["NumSharedGenes"].astype(int)
 res.groupby(['Model Type', 'imp_type', 'Comparison'])[
-    'Num Shared Genes'].describe()  # all envs
-#                                   count         mean          std     min     25%     50%     75%     max
-# Model Type imp_type Comparison
-# FS         SHAP     PAVs vs CNVs   35.0    33.485714    31.307180     0.0    12.0    28.0    40.5   121.0
-#                     SNPs vs CNVs   35.0     2.971429     4.859713     0.0     0.0     1.0     3.0    21.0
-#                     SNPs vs PAVs   35.0     2.057143     3.271471     0.0     0.0     1.0     2.0    13.0
-#            gini     PAVs vs CNVs   35.0    33.485714    31.307180     0.0    12.0    28.0    40.5   121.0
-#                     SNPs vs CNVs   35.0     3.000000     4.970501     0.0     0.0     1.0     3.0    22.0
-#                     SNPs vs PAVs   35.0     2.057143     3.271471     0.0     0.0     1.0     2.0    13.0
-# baseline   SHAP     PAVs vs CNVs   35.0   906.400000   394.296186    85.0   699.5   922.0  1097.0  1755.0
-#                     SNPs vs CNVs   35.0  1515.885714   939.633463    28.0   932.5  1715.0  2086.0  3734.0
-#                     SNPs vs PAVs   35.0   296.342857   165.704014     8.0   188.0   313.0   396.0   625.0
-#            gini     PAVs vs CNVs   35.0  1990.571429   470.569573   575.0  1889.0  2145.0  2277.0  2473.0
-#                     SNPs vs CNVs   35.0  4550.142857  1210.454731  1009.0  4789.0  5091.0  5242.5  5359.0
-#                     SNPs vs PAVs   35.0   655.857143   157.088050   158.0   682.5   728.0   738.5   747.0
-
+    'NumSharedGenes'].describe()  # all envs
+'''                               count         mean          std     min     25%     50%     75%     max
+Model Type imp_type Comparison                                                                           
+complete   SHAP     PAVs vs CNVs   35.0   906.400000   394.296186    85.0   699.5   922.0  1097.0  1755.0
+                    SNPs vs CNVs   35.0  1480.542857   909.307472    33.0   922.5  1730.0  2055.0  3726.0
+                    SNPs vs PAVs   35.0   301.171429   167.063300    14.0   197.5   316.0   403.0   621.0
+           gini     PAVs vs CNVs   35.0  1990.571429   470.569573   575.0  1889.0  2145.0  2277.0  2473.0
+                    SNPs vs CNVs   35.0  4539.457143  1226.998258  1010.0  4775.0  5089.0  5238.0  5361.0
+                    SNPs vs PAVs   35.0   656.342857   157.887328   148.0   686.0   730.0   739.0   747.0
+optimized  SHAP     PAVs vs CNVs   35.0    33.485714    31.307180     0.0    12.0    28.0    40.5   121.0
+                    SNPs vs CNVs   35.0     3.257143     6.035887     0.0     0.0     1.0     3.0    24.0
+                    SNPs vs PAVs   35.0     2.400000     3.126735     0.0     0.0     2.0     3.0    12.0
+           gini     PAVs vs CNVs   35.0    33.485714    31.307180     0.0    12.0    28.0    40.5   121.0
+                    SNPs vs CNVs   35.0     3.257143     6.035887     0.0     0.0     1.0     3.0    24.0
+                    SNPs vs PAVs   35.0     2.400000     3.126735     0.0     0.0     2.0     3.0    12.0
+'''
 # Get the average overlap for target environments only
 res_sub = res[res.Env.isin(['YPDCAFEIN40', 'YPDCAFEIN50', 'YPDBENOMYL500',
                             'YPDCUSO410MM', 'YPDSODIUMMETAARSENITE'])]  # target envs only
 res_sub.groupby(['Model Type', 'imp_type', 'Comparison'])[
-    'Num Shared Genes'].describe()
-#                                   count    mean          std     min     25%     50%     75%     max
-# Model Type imp_type Comparison
-# FS         SHAP     PAVs vs CNVs    5.0    38.4    22.941229     8.0    28.0    39.0    47.0    70.0
-#                     SNPs vs CNVs    5.0     5.4     4.037326     1.0     3.0     4.0     8.0    11.0
-#                     SNPs vs PAVs    5.0     7.2     5.761944     1.0     2.0     7.0    13.0    13.0
-#            gini     PAVs vs CNVs    5.0    38.4    22.941229     8.0    28.0    39.0    47.0    70.0
-#                     SNPs vs CNVs    5.0     5.4     4.037326     1.0     3.0     4.0     8.0    11.0
-#                     SNPs vs PAVs    5.0     7.2     5.761944     1.0     2.0     7.0    13.0    13.0
-# baseline   SHAP     PAVs vs CNVs    5.0  1012.0   315.167416   514.0   959.0  1090.0  1129.0  1368.0
-#                     SNPs vs CNVs    5.0  1565.8   841.145469   544.0  1299.0  1362.0  1783.0  2841.0
-#                     SNPs vs PAVs    5.0   384.4    79.178911   265.0   376.0   391.0   404.0   486.0
-#            gini     PAVs vs CNVs    5.0  2097.6   321.458862  1575.0  2123.0  2145.0  2188.0  2457.0
-#                     SNPs vs CNVs    5.0  4654.0  1025.237777  2832.0  4976.0  5055.0  5113.0  5294.0
-#                     SNPs vs PAVs    5.0   738.6     3.577709   735.0   736.0   738.0   740.0   744.0
+    'NumSharedGenes'].describe()
+'''                               count    mean          std     min     25%     50%     75%     max
+Model Type imp_type Comparison                                                                      
+complete   SHAP     PAVs vs CNVs    5.0  1012.0   315.167416   514.0   959.0  1090.0  1129.0  1368.0
+                    SNPs vs CNVs    5.0  1539.2   784.753592   549.0  1265.0  1389.0  1808.0  2685.0
+                    SNPs vs PAVs    5.0   393.6    82.530600   276.0   370.0   407.0   410.0   505.0
+           gini     PAVs vs CNVs    5.0  2097.6   321.458862  1575.0  2123.0  2145.0  2188.0  2457.0
+                    SNPs vs CNVs    5.0  4653.8  1026.038596  2831.0  4976.0  5056.0  5104.0  5302.0
+                    SNPs vs PAVs    5.0   739.4     5.899152   730.0   739.0   740.0   742.0   746.0
+optimized  SHAP     PAVs vs CNVs    5.0    38.4    22.941229     8.0    28.0    39.0    47.0    70.0
+                    SNPs vs CNVs    5.0     4.8     5.449771     0.0     0.0     4.0     7.0    13.0
+                    SNPs vs PAVs    5.0     6.6     4.615192     2.0     3.0     5.0    11.0    12.0
+           gini     PAVs vs CNVs    5.0    38.4    22.941229     8.0    28.0    39.0    47.0    70.0
+                    SNPs vs CNVs    5.0     4.8     5.449771     0.0     0.0     4.0     7.0    13.0
+                    SNPs vs PAVs    5.0     6.6     4.615192     2.0     3.0     5.0    11.0    12.0
+'''
