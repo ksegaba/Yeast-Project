789c44f1780791b602c98ffdb1586d775af987c5
ksegaba <keniaesegura@gmail.com>
2025-09-11 09:29:55 -0400

    autopep8 formatting & updated working directory path

--- Scripts/Data_Vis/0_Figure_S3_fitness_distributions.py
+++ Scripts/Data_Vis/0_Figure_S3_fitness_distributions.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-os.chdir("/mnt/home/seguraab/Shiu_Lab/Project")
+os.chdir("/mnt/research/glbrc_group/shiulab/kenia/Shiu_Lab/Project")
 
 
 pheno = pd.read_csv("Data/Peter_2018/pheno.csv", index_col=0)  # fitness data
@@ -16,22 +16,25 @@ test = pd.read_csv("Data/Peter_2018/Test.txt", header=None)  # test set data
 pheno_train = pheno.loc[~pheno.index.isin(test[0].tolist())]
 pheno_test = pheno.loc[pheno.index.isin(test[0].tolist())]
 
-## S3. Training and test set distributions on one plot
-fig, ax = plt.subplots(7, 5, figsize=(8.5, 11))
+# S3. Training and test set distributions on one plot
+fig, ax = plt.subplots(7, 5, figsize=(10.5, 13), sharex=True)
 ax = ax.flatten()
 for i, col in enumerate(pheno.columns):
-	pheno_train[col].plot.hist(bins=30, alpha=0.5, ax=ax[i], label="Training set")
-	pheno_test[col].plot.hist(bins=30, alpha=0.5, ax=ax[i], label="Test set")
-	ax[i].set_xlabel("Fitness")
-	ax[i].set_ylabel("Frequency")
-	ax[i].set_title(col)
+    pheno_train[col].plot.hist(
+        bins=30, alpha=0.5, ax=ax[i], label="Training set", color="#38cbff")
+    pheno_test[col].plot.hist(bins=30, alpha=0.5, ax=ax[i], label="Test set",
+                              color="#0053a1")
+    ax[i].set_xlabel("Fitness")
+    ax[i].set_ylabel("Frequency")
+    ax[i].set_title(col)
 
 
 plt.tight_layout()
-plt.savefig("Scripts/Data_Vis/Section_3/Figure_S3_train_test_pheno.pdf", bbox_inches="tight")
+plt.savefig("Scripts/Data_Vis/Section_3/Figure_S3_train_test_pheno.pdf",
+            bbox_inches="tight")
 plt.close()
 
-## S3. Violin plot of fitness distributions in each environment
+# S3. Violin plot of fitness distributions in each environment
 fig, ax = plt.subplots(figsize=(10, 4))
 sns.violinplot(data=pheno, ax=ax, linewidth=0.7)
 ax.tick_params(axis='x', rotation=90)
