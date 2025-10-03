09bbbe5dd743a540d8d5965c26cc42f4eacea620
ksegaba <keniaesegura@gmail.com>
2025-10-03 14:18:53 -0400

    add go, pwy, bench gene annotations to feature importance files

--- Scripts/Data_Vis/0_Table_S5_S9_SFile7_gini_vs_shap_rho.py
+++ Scripts/Data_Vis/0_Table_S5_S9_SFile7_gini_vs_shap_rho.py
@@ -2,9 +2,11 @@
 
 ################################################################################
 # In this script, I do the following:
-# 1. Combine the average Gini importances for all environments (Table S9 & Supplementary data file 7)
-# 2. Combine the average absolute SHAP values for all environments (Table S9 & Supplementary data file 7)
-# 3. Calculate spearman's rho correlations between Gini and SHAP values for each environment (Table S5)
+# 1. Combine the average Gini importances for all environments (Table S9 & Supplementary file 7)
+# 2. Combine the average absolute SHAP values for all environments (Table S9 & Supplementary file 7)
+# 3. Add GO terms, pathways, and benchmark gene info to Table S9 and Supplementary file 7
+#    Also add the annotations to the feature to gene mapping files (Supplementary files 8 & 9)
+# 4. Calculate spearman's rho correlations between Gini and SHAP values for each environment (Table S5)
 ################################################################################
 
 import os
@@ -193,7 +195,125 @@ combine_shap_indiv(cnv_shap_complete_files, map=map_orfs, dtype="cnv",
                    save="Scripts/Data_Vis/Section_3/RF_complete_shap_cnv.tsv")
 
 ################################################################################
-# 3. Calculate spearman's rho correlations between Gini and SHAP values for each environment.
+# 3. Add GO terms, pathways, and benchmark gene info to Table S9 and Supplementary file 7
+################################################################################
+# Read in the annotation files
+go = pd.read_csv("Data/yeast_GO/sgd_GO_BP_no_info.txt", sep="\t")
+pwy_orf = pd.read_csv(
+    "Data/Peter_2018/ORFs_and_S288C_genes_pwy_all_CORRECTED_16_removed.csv")
+pwy_snp = pd.read_csv(
+    "Data/Peter_2018/biallelic_snps_diploid_and_S288C_genes_CORRECTED_pathway_map.csv")
+map_snps = pd.read_csv(
+    "Data/Peter_2018/biallelic_snps_diploid_and_S288C_genes_CORRECTED_expanded_benchmark.tsv", sep="\t")
+map_orfs = pd.read_csv(
+    "/mnt/research/glbrc_group/shiulab/kenia/Shiu_Lab/Project/Data/Peter_2018/final_map_orf_to_gene_CORRECTED_16_removed_expanded_benchmark.tsv", sep="\t")
+curated_snp = pd.read_csv(
+    "Data/SGD_Experiment_Genes/manually_curated_genes_snps.txt", sep="\t")
+curated_orf = pd.read_csv(
+    "Data/SGD_Experiment_Genes/manually_curated_genes_orfs.txt", sep="\t")
+
+# Delimet all the annotations with "; " for each gene
+go_agg = go.groupby("Gene")["GO.ID"].agg(lambda x: "; ".join(x)).reset_index()
+pwy_orf = pwy_orf[["Accession.1", "Pathways.of.gene"]].astype(str)
+pwy_orf_agg = pwy_orf.groupby("Accession.1")["Pathways.of.gene"].agg(
+    lambda x: "; ".join(x)).reset_index()
+pwy_snp = pwy_snp[["gene", "pathway"]].astype(str)
+pwy_snp_agg = pwy_snp.groupby("gene")["pathway"].agg(
+    lambda x: "; ".join(x)).reset_index()
+
+# Order of the environments to save the columns as in the final tables
+env_order = pd.read_csv(
+    "Scripts/Data_Vis/Section_2/Figure_2_r2_test_v5_env_order.txt", sep="\t", header=None)
+inverted_mapping = {v: k for k, v in mapping.items()}
+env_order["Envs"] = env_order[0].map(inverted_mapping)
+
+# Read in the combined Gini and SHAP files and add annotations
+for data_type in ["snp", "pav", "cnv"]:
+    for model_type in ["optimized", "complete"]:
+        for imp_type in ["shap", "gini"]:
+            df = dt.fread(
+                f"Scripts/Data_Vis/Section_3/RF_{model_type}_{imp_type}_{data_type}.tsv").to_pandas()
+            # add GO terms
+            df = df.merge(go_agg, how="left", left_on="gene", right_on="Gene")
+            # add pathways and benchmark gene info
+            if data_type != "snp":
+                df = df.merge(pwy_orf_agg, how="left",
+                              left_on="gene", right_on="Accession.1")
+                df = df.merge(map_orfs[["orf", "Benomyl", "Caffeine", "CuSO4",
+                                        "Sodium_meta-arsenite"]], how="left",
+                              left_on="orf", right_on="orf")
+                # re-order the df columns
+                df = df[["orf", "gene", "GO.ID", "Pathways.of.gene", "Benomyl",
+                        "Caffeine", "CuSO4", "Sodium_meta-arsenite"] +
+                        env_order["Envs"].tolist()]
+                df.insert(8, "Curated", np.where(
+                    df["gene"].isin(curated_orf), 1, 0))
+            else:
+                df = df.merge(pwy_snp_agg, how="left",
+                              left_on="gene", right_on="gene")
+                df = df.merge(map_snps[["snp", "Benomyl", "Caffeine", "CuSO4",
+                                        "Sodium_meta-arsenite"]], how="left",
+                              left_on="snp", right_on="snp")
+                # re-order the df columns
+                df = df[["snp", "gene", "GO.ID", "pathway", "Benomyl",
+                         "Caffeine", "CuSO4", "Sodium_meta-arsenite"] +
+                        env_order["Envs"].tolist()]
+                df.insert(8, "Curated", np.where(
+                    df["gene"].isin(curated_snp), 1, 0))
+            #
+            # Save the updated files
+            df.to_csv(
+                f"Scripts/Data_Vis/Section_3/RF_{model_type}_{imp_type}_{data_type}_with_annotations.tsv", sep="\t", index=False)
+            del df
+
+
+# Add annotations to the feature to gene mapping files (Supplementary data files 8 & 9)
+map_snps.insert(8, "Curated", np.where(
+    map_snps["gene"].isin(curated_snp), 1, 0))
+map_orfs.insert(7, "Curated", np.where(
+    map_orfs["gene"].isin(curated_orf), 1, 0))
+map_snps["Curated"].sum()  # 0, so drop
+map_snps.drop(columns=["Curated"], inplace=True)
+map_orfs["Curated"].sum()  # 4, so drop
+map_orfs.drop(columns=["Curated"], inplace=True)
+map_snps = map_snps.merge(go_agg, how="left", left_on="gene", right_on="Gene")
+map_snps = map_snps.merge(pwy_snp_agg, how="left",
+                          left_on="gene", right_on="gene")
+map_orfs = map_orfs.merge(go_agg, how="left", left_on="gene", right_on="Gene")
+map_orfs = map_orfs.merge(pwy_orf_agg, how="left",
+                          left_on="gene", right_on="Accession.1")
+map_snps.to_csv(
+    "Data/Peter_2018/biallelic_snps_diploid_and_S288C_genes_CORRECTED_expanded_benchmark_with_annotations.tsv", sep="\t", index=False)
+map_orfs.to_csv(
+    "Data/Peter_2018/final_map_orf_to_gene_CORRECTED_16_removed_expanded_benchmark_with_annotations.tsv", sep="\t", index=False)
+
+map_snps.isna().sum()
+# snp                         0
+# chr                         0
+# pos                         0
+# gene                        0
+# Benomyl                     0
+# Caffeine                    0
+# CuSO4                       0
+# Sodium_meta-arsenite        0
+# Gene                    49971
+# GO.ID                   49971
+# pathway                 45585
+map_orfs.isna().sum()
+# orf                       0
+# gene                      0
+# organism                  0
+# Benomyl                   0
+# Caffeine                  0
+# CuSO4                     0
+# Sodium_meta-arsenite      0
+# Gene                    759
+# GO.ID                   759
+# Accession.1             351
+# Pathways.of.gene        351
+
+################################################################################
+# 4. Calculate spearman's rho correlations between Gini and SHAP values for each environment.
 ################################################################################
 res = [["Model Type", "Data Type", "Env", "rho", "pval", "NumShared"]]
 for data_type in ["snp", "pav", "cnv"]:
@@ -261,7 +381,7 @@ for data_type in ["snp", "pav", "cnv"]:
         # Calculate spearman's rho
         # First, for the optimized RF model features
         df = pd.concat([gini_env_rank_out.loc[:, env],
-                       shap_env_rank_out.loc[:, env]], ignore_index=False, axis=1).dropna()
+                        shap_env_rank_out.loc[:, env]], ignore_index=False, axis=1).dropna()
         rho = df.corr(method=lambda x, y: spearmanr(
             x, y, alternative="two-sided").statistic)
         pval = df.corr(method=lambda x, y: spearmanr(
@@ -270,7 +390,7 @@ for data_type in ["snp", "pav", "cnv"]:
                    pval.iloc[0, 1], len(df)])
         # Second, for the complete RF model features
         df = pd.concat([gini_comp_rank_out.loc[:, env],
-                       shap_comp_rank_out.loc[:, env]], ignore_index=False, axis=1).dropna()
+                        shap_comp_rank_out.loc[:, env]], ignore_index=False, axis=1).dropna()
         rho = df.corr(method=lambda x, y: spearmanr(
             x, y, alternative="two-sided").statistic)
         pval = df.corr(method=lambda x, y: spearmanr(
@@ -296,32 +416,32 @@ pav = pd.read_csv(
 cnv = pd.read_csv(
     'Scripts/Data_Vis/Section_2/RESULTS_RF_CNVs_FS.txt', sep='\t', index_col='Y')
 
-r2_v_rho_snp = rho[(rho['Model Type'] == 'optimized') & (rho['Data Type'] == 'snp')].\
-    merge(snp['r2_test'], right_index=True, left_on="Env")
+r2_v_rho_snp = rho[(rho['Model Type'] == 'optimized') & (rho['Data Type'] == 'snp')].
+merge(snp['r2_test'], right_index=True, left_on="Env")
 # np.float64(-0.032360053556767396)
 r2_v_rho_snp[['rho', 'r2_test']].corr().iloc[0, 1]
 
-r2_v_rho_pav = rho[(rho['Model Type'] == 'optimized') & (rho['Data Type'] == 'pav')].\
-    merge(pav['r2_test'], right_index=True, left_on="Env")
+r2_v_rho_pav = rho[(rho['Model Type'] == 'optimized') & (rho['Data Type'] == 'pav')].
+merge(pav['r2_test'], right_index=True, left_on="Env")
 # np.float64(0.3300096647369571)
 r2_v_rho_pav[['rho', 'r2_test']].corr().iloc[0, 1]
 
-r2_v_rho_cnv = rho[(rho['Model Type'] == 'optimized') & (rho['Data Type'] == 'cnv')].\
-    merge(cnv['r2_test'], right_index=True, left_on="Env")
+r2_v_rho_cnv = rho[(rho['Model Type'] == 'optimized') & (rho['Data Type'] == 'cnv')].
+merge(cnv['r2_test'], right_index=True, left_on="Env")
 # np.float64(0.287642642641329)
 r2_v_rho_cnv[['rho', 'r2_test']].corr().iloc[0, 1]
 
-r2_v_rho_snp = rho[(rho['Model Type'] == 'complete') & (rho['Data Type'] == 'snp')].\
-    merge(snp['r2_test'], right_index=True, left_on="Env")
+r2_v_rho_snp = rho[(rho['Model Type'] == 'complete') & (rho['Data Type'] == 'snp')].
+merge(snp['r2_test'], right_index=True, left_on="Env")
 # np.float64(-0.370472133387726)
 r2_v_rho_snp[['rho', 'r2_test']].corr().iloc[0, 1]
 
-r2_v_rho_pav = rho[(rho['Model Type'] == 'complete') & (rho['Data Type'] == 'pav')].\
-    merge(pav['r2_test'], right_index=True, left_on="Env")
+r2_v_rho_pav = rho[(rho['Model Type'] == 'complete') & (rho['Data Type'] == 'pav')].
+merge(pav['r2_test'], right_index=True, left_on="Env")
 # np.float64(0.2079527477389604)
 r2_v_rho_pav[['rho', 'r2_test']].corr().iloc[0, 1]
 
-r2_v_rho_cnv = rho[(rho['Model Type'] == 'complete') & (rho['Data Type'] == 'cnv')].\
-    merge(cnv['r2_test'], right_index=True, left_on="Env")
+r2_v_rho_cnv = rho[(rho['Model Type'] == 'complete') & (rho['Data Type'] == 'cnv')].
+merge(cnv['r2_test'], right_index=True, left_on="Env")
 # np.float64(0.3529860550102224)
 r2_v_rho_cnv[['rho', 'r2_test']].corr().iloc[0, 1]
