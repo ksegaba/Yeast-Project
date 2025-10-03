0c2f2d9ec0c2ea84605ac095d6a802da8d53c4b3
ksegaba <keniaesegura@gmail.com>
2025-10-03 14:10:04 -0400

    clean up model slurm submission scripts

--- Job_Submission_Scripts/SNPs_as_Feat/RF/RF_job_submission.sh
+++ Job_Submission_Scripts/SNPs_as_Feat/RF/RF_job_submission.sh
@@ -1,5 +1,5 @@
 #!/bin/bash --login 
-#SBATCH --array=1-35
+#SBATCH --array=33-34
 #SBATCH --time=50:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=2
@@ -8,18 +8,19 @@
 #SBATCH --job-name baseline_rf
 #SBATCH --output=%x_%j.out
 
-cd /mnt/scratch/seguraab/yeast_project/SNP_yeast_RF_results/baseline
+cd /mnt/research/glbrc_group/shiulab/kenia/yeast_project/SNP_yeast_RF_results/baseline
 
 module purge
 conda activate /mnt/home/seguraab/miniconda3/envs/ml-pipeline
 TRAITS=($(head -n 1 ~/Shiu_Lab/Project/Data/Peter_2018/pheno.csv | sed 's/,/ /g'))
+
 echo "${SLURM_ARRAY_TASK_ID} ; ${TRAITS[${SLURM_ARRAY_TASK_ID}]}"
 
 # Baseline models
-#echo "Baseline for ${TRAITS[${SLURM_ARRAY_TASK_ID}]}"
+echo "Baseline for ${TRAITS[${SLURM_ARRAY_TASK_ID}]}"
 path1=/mnt/home/seguraab/Shiu_Lab/Project/External_software/ML-Pipeline
 data=/mnt/home/seguraab/Shiu_Lab/Project/Data/Peter_2018
-python ${path1}/ML_regression.py -df ${data}/geno.csv -df2 ${data}/pheno.csv -y_name ${TRAITS[${SLURM_ARRAY_TASK_ID}]} -sep , -test ${data}/Test.txt -alg RF -n_jobs 12 -n 10 -cv_num 5 -save ${TRAITS[${SLURM_ARRAY_TASK_ID}]}_rf_baseline -plots t
+python ${path1}/ML_regression.py -df ${data}/geno.csv -df2 ${data}/pheno.csv -y_name ${TRAITS[${SLURM_ARRAY_TASK_ID}]} -sep , -test ${data}/Test.txt -alg RF -n_jobs 12 -n 20 -cv_num 5 -save ${TRAITS[${SLURM_ARRAY_TASK_ID}]}_rf_baseline -plots t
 
 conda deactivate
 
