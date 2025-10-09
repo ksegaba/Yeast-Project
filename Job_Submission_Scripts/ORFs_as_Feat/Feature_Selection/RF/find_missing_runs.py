daee071e112e09a1c442b5f6eb696c695d5820b5
ksegaba <keniaesegura@gmail.com>
2025-10-08 20:35:59 -0400

    clean up scripts

--- Job_Submission_Scripts/ORFs_as_Feat/Feature_Selection/RF/find_missing_runs.py
+++ Job_Submission_Scripts/ORFs_as_Feat/Feature_Selection/RF/find_missing_runs.py
@@ -4,14 +4,13 @@ import os
 import re
 import pandas as pd
 
-os.chdir('/mnt/gs21/scratch/seguraab/yeast_project/ORF_yeast_RF_results/fs')
+os.chdir('/mnt/research/glbrc_group/shiulab/kenia/yeast_project/ORF_yeast_RF_results/fs')
 
 # check to make sure no old files are present
-os.system('ls -l | grep -P May')
+os.system('ls -l | grep -P Aug')
 
 # results summary file
 res = pd.read_csv("RESULTS_reg.txt", sep="\t")
-res = res.loc[res.DateTime.str.contains("2024-06"),:]
 
 # get the list of feature selection files that did not run
 path = '/mnt/home/seguraab/Shiu_Lab/Project/Job_Submission_Scripts/ORFs_as_Feat/Feature_Selection/RF/RF_FS_runs.txt'
@@ -19,7 +18,13 @@ runs = open(path, 'r').read().split('\n')
 
 with open(path.replace('RF_FS_runs.txt','RF_FS_missing_runs.txt'), 'w') as f:
     for run in runs[:2800]:
-        feat_file = run.split(' ')[11].split('/fs/')[1]
+        try:
+            feat_file = run.split(' ')[11].split('/fs/')[1]
+        except IndexError:
+            try:
+                feat_file = run.split(' ')[12].split('/fs/')[1]
+            except IndexError:
+                feat_file = run.split(' ')[13].split('/fs/')[1]
         # check if the _imp file exists (since it's created after all training reps)
         feat_num = re.search(r'top_[0-9]+', feat_file).group()
         env = re.search(r'(?<=_)[A-Z0-9]+(?=_)', feat_file).group()
@@ -31,5 +36,5 @@ with open(path.replace('RF_FS_runs.txt','RF_FS_missing_runs.txt'), 'w') as f:
             f.write(f'{run}\n')
         del run, feat_file, feat_num, env, dat_type
 
-
-# June 11. there are no missing pav/cnv runs!
+# June 11. there are 3 missing runs
+# July 17. there are no missing pav/cnv runs!
