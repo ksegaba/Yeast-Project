f284866a65415f497818b9a6652be34d2ac8635c
ksegaba <keniaesegura@gmail.com>
2025-10-20 13:37:09 -0400

    impute SNP genotypes and create a csv file (-1,0,1 or 0,1,2)

--- Scripts/Data_Processing/fastPHASE_to_csv.py
+++ Scripts/Data_Processing/fastPHASE_to_csv.py
@@ -7,16 +7,22 @@ Note: The minor allele of the fastPHASE file is encoded as 1 and
     the major allele is encoded as 2.
 
 Example:
-    path=/mnt/home/seguraab/Shiu_Lab/Project/Data/Peter_2018/
+    # path=/mnt/home/seguraab/Shiu_Lab/Project/Data/Peter_2018/
+    path=/mnt/research/glbrc_group/shiulab/kenia/Shiu_Lab/Project/Data/Peter_2018/0_raw_data
     file=1011Matrix_diploid750_biallelic_maf0.05_11032022_genotypes.out
-    map=1011Matrix_diploid750_biallelic_maf0.05_11032022.plink.map
-    save=1011Matrix_diploid750_biallelic_maf0.05_11032022_genotypes.csv
-    python fastPHASE_to_csv.py -path $path -file $file -map $map -save $save
+    # map=1011Matrix_diploid750_biallelic_maf0.05_11032022.plink.map
+    map=1011Matrix_diploid750_biallelic_maf0.05_09102025.fastphase.frq
+    recoded=1011Matrix_diploid750_biallelic_maf0.05_11032022.fastphase.chr-0.recode.phase.inp
+    # save=1011Matrix_diploid750_biallelic_maf0.05_11032022_genotypes.csv
+    save=1011Matrix_diploid750_biallelic_maf0.05_09102025_genotypes.csv
+    # python fastPHASE_to_csv.py -path $path -file $file -map $map -save $save
+    python fastPHASE_to_csv.py -path $path -file $file -map $map -recoded $recoded -save $save
     
 Args:
     path (str): Path to working directory.
     file (str): Name of fastPHASE imputed _genotype.out file.
     map (str): Name of PLINK map file containing chromosome and bp position of genotypes.
+    recoded (str): Name of the PLINK output file after recoding into major/minor alleles in the fastphase format.
     save (str): Name to save matrix file as.
 
 Returns:
@@ -31,7 +37,8 @@ Reference:
 
 __author__ = "Kenia Segura Abá"
 
-import sys, os
+import sys
+import os
 import argparse
 import warnings
 import pandas as pd
@@ -42,42 +49,54 @@ import pyspark
 
 def warn(*args, **kwargs):
     pass
+
+
 warnings.warn = warn
 
 
-def fastPHASE_to_csv(inf, map, save):
+def fastPHASE_to_csv(inf, recoded, map, save):
     """ Convert fastPHASE imputed biallelic genotypes file to CSV """
-    
-    # read line by line
-    inl = inf.readline() # read the first line of the file
-    
-    geno = {} # dict of IDs and recoded genotypes
-    
-    with tqdm(total=751) as pbar: # progress bar
+    # read the PLINK recoded genotypes file to get the genotype order
+    recoded_inl = recoded.readline()
+    while not recoded_inl.strip().startswith("P"):
+        recoded_inl = recoded.readline()
+
+    # fetch the snp IDs based on the order in the recoded file
+    recoded_inl = pd.Series(
+        recoded_inl.strip().split(" ")[1:])  # skip the "P" at index 0
+    recoded_inl.name = "P"
+    assert recoded_inl.eq(map["PLINK_ID"]).all()  # sanity check, returns True!
+
+    # read the imputed genotypes line by line
+    inl = inf.readline()  # read the first line of the file
+
+    geno = {}  # dict of IDs and recoded genotypes
+
+    with tqdm(total=751) as pbar:  # progress bar
         while inl.strip() != "END GENOTYPES":
             if inl.strip() == "BEGIN GENOTYPES":
                 pass
 
             if inl.strip().startswith("#"):
                 # fetch individual IDs
-                ID = inl.strip().split(" ")[2] # individual ID
-                if ID not in geno.keys(): # check if ID exists
+                ID = inl.strip().split(" ")[2]  # individual ID
+                if ID not in geno.keys():  # check if ID exists
                     geno[ID] = {}
 
                 # fetch genotypes for all SNPs
-                inl = inf.readline().strip().split(" ") # allele 1
-                inl2 = inf.readline().strip().split(" ") # allele 2
+                inl = inf.readline().strip().split(" ")  # allele 1
+                inl2 = inf.readline().strip().split(" ")  # allele 2
 
                 # recode SNP genotype
                 for i in range(len(inl)):
-                    snp = map.iloc[i,1] # SNP name
-                    snp = snp.replace(":", "_") # change to chr_pos format
+                    # SNP name ---------------------------------This is where I went wrong
+                    snp = map.iloc[i, 6]  # fetch SNP ID in chr_pos format
                     if snp not in geno[ID].keys():
-                        if inl[i]=="2" and inl2[i]=="2": # homozygous major
+                        if inl[i] == "2" and inl2[i] == "2":  # homozygous major
                             geno[ID][snp] = -1
-                        elif inl[i]!=inl2[i]: # heterozygous
+                        elif inl[i] != inl2[i]:  # heterozygous
                             geno[ID][snp] = 0
-                        elif inl[i]=="1" and inl2[i]=="1": # homozygous minor
+                        elif inl[i] == "1" and inl2[i] == "1":  # homozygous minor
                             geno[ID][snp] = 1
             inl = inf.readline()
             pbar.update(1)
@@ -86,31 +105,46 @@ def fastPHASE_to_csv(inf, map, save):
     # save recoded genotype matrix to file
     geno = pd.DataFrame(geno)
     geno = geno.T
-    geno.rename(columns={'index':'ID'}, inplace=True)
+    geno.rename(columns={'index': 'ID'}, inplace=True)
     geno.reset_index(inplace=True)
     geno = dt.Frame(geno)
     geno.to_csv(save, quoting="none")
-    return(0)
+    return (0)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # Argument parser
-    parser = argparse.ArgumentParser(description="Test script to convert vcf to matrix format")
+    parser = argparse.ArgumentParser(
+        description="Test script to convert vcf to matrix format")
     req_group = parser.add_argument_group(title="REQUIRED INPUT")
-    req_group.add_argument("-path", type=str, help="Path to working directory.", required=True)
-    req_group.add_argument("-file", type=str, help="Name of fastPHASE imputed _genotype.out file in the working directory.", required=True)
-    req_group.add_argument("-map", type=str, help="Name of PLINK map file containing chromosome and bp position of genotypes.")
-    req_group.add_argument("-save", type=str, help="Name to save matrix file as.", required=True)
-    if len(sys.argv)==1:
+    req_group.add_argument("-path", required=True, type=str,
+                           help="Path to working directory.")
+    req_group.add_argument("-file", required=True, type=str,
+                           help="Name of fastPHASE imputed _genotype.out file in the working directory.")
+    req_group.add_argument("-map", required=True, type=str,
+                           help="Name of PLINK .frq file containing chromosome and bp position of genotypes. It contains the SNP ID order in the recoded file.")
+    req_group.add_argument("-recoded", required=True, type=str,
+                           help="Name of the PLINK output file after recoding into major/minor alleles in the fastphase format.")
+    req_group.add_argument("-save", required=True, type=str,
+                           help="Name to save matrix file as.")
+    if len(sys.argv) == 1:
         parser.print_help()
         sys.exist(0)
     args = parser.parse_args()
 
     # Set working directory
     os.chdir(args.path)
-    
-    # Read in fastPHASE imputed genotypes file & PLINK mapping file
+
+    # Read in the recoded fastPHASE file, imputed genotypes file, & PLINK mapping file
     inf = open(args.file, "r")
-    map = pd.read_csv(args.map, sep="\t", header=None)
-    fastPHASE_to_csv(inf=inf, map=map, save=args.save) # convert to CSV
+    map = dt.fread(args.map).to_pandas()
+    recoded = open(args.recoded, "r")
+
+    # Add the SNP IDs to the map file
+    map["ID"] = map["SNP"].str.replace(":", "_")  # change to chr_pos format
+    map["PLINK_ID"] = map["SNP"].str.split(":", expand=True)[1]
+
+    # Convert fastPHASE imputed genotypes to CSV
+    fastPHASE_to_csv(inf=inf, recoded=recoded, map=map,
+                     save=args.save)  # convert to CSV
     inf.close()
